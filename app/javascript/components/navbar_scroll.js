ca5bb231cdc65de3fbf89b0c099b0435cd8a1576
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-09-05 12:21:55 +0200

    style research and refine navbar behavior for all sizes of devices

--- app/javascript/components/navbar_scroll.js
+++ app/javascript/components/navbar_scroll.js
@@ -2,7 +2,7 @@ function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.nav');
   if (navbar) {
     navbar.style.transition = "all 0.3s";
-    if (window.innerWidth >= 1024) {
+    if (window.innerWidth >= 50) {
       window.addEventListener('scroll', () => {
         if (window.scrollY >= 50) {
 
