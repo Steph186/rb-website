8c5d9e3e626cae5e90ed4c3a07c3c50e620ff9c6
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-09-09 16:40:48 +0200

    change focus name

--- db/migrate/20180824075121_create_pictures.rb
+++ db/migrate/20180824075121_create_pictures.rb
@@ -3,7 +3,7 @@ class CreatePictures < ActiveRecord::Migration[5.2]
     create_table :pictures do |t|
       t.string :page
       t.string :photo, default: "https://picsum.photos/200/300", null: false
-      t.string :focus, default: '0%', null: false
+      t.string :focus_picture, default: '0%', null: false
 
       t.timestamps
     end
