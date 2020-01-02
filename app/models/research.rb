f5e4d75a150ef4d14de6c188adea33daae43f535
Steph186 <stephanie.cattoir@gmail.com>
2020-01-02 22:32:30 +0100

    Add fare image

--- app/models/research.rb
+++ app/models/research.rb
@@ -1,5 +1,5 @@
 class Research < ApplicationRecord
   ICONS_TOPICS = ["1 Book", "2 Language", "3 Brain"]
   ICONS_LINKS = ["Research Gate", "Twitter", "GitHub", "Google Scholar", "Slack", "Cannabis"]
-  IMAGES_FUNDING = ["ERC.jpg", "PRIN.jpg", "MCURIE.jpg"]
+  IMAGES_FUNDING = ["ERC.jpg", "PRIN.jpg", "MCURIE.jpg", "FARE.png"]
 end
